fa070997c92f8acbad68ddaa05dcb28be82b76f8
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2021-09-16 15:06:07 -0700

    enter

--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -3,7 +3,7 @@
 The public version of the file used for testing can be found here: https://gist.github.com/ConsenSys-Academy/ce47850a8e2cba6ef366625b665c7fba
 
 This test file has been updated for Truffle version 5.0. If your tests are failing, make sure that you are
-using Truffle version 5.0. You can check this by running "trufffle version"  in the terminal. If version 5 is not
+using Truffle version 5.0. You can check this by running "truffle version"  in the terminal. If version 5 is not
 installed, you can uninstall the existing version with `npm uninstall -g truffle` and install the latest version (5.0)
 with `npm install -g truffle`.
 
@@ -19,7 +19,7 @@ contract("SimpleBank", function (accounts) {
     instance = await SimpleBank.new();
   });
 
-  it("ready to be solved!", async() => {
+  it("ready to be solved!", async () => {
     const eth100 = 100e18;
     assert.equal(await web3.eth.getBalance(alice), eth100.toString());
   });
@@ -34,7 +34,7 @@ contract("SimpleBank", function (accounts) {
       //   2: https://docs.soliditylang.org/en/v0.8.5/contracts.html#getter-functions
       await instance.owner.call(),
       contractOwner,
-      "owner is not correct",
+      "owner is not correct"
     );
   });
 
@@ -45,16 +45,18 @@ contract("SimpleBank", function (accounts) {
     assert.equal(
       aliceEnrolled,
       true,
-      "enroll balance is incorrect, check balance method or constructor",
+      "enroll balance is incorrect, check balance method or constructor"
     );
   });
 
   it("should not mark unenrolled users as enrolled", async () => {
-    const ownerEnrolled = await instance.enrolled(contractOwner, { from: contractOwner });
+    const ownerEnrolled = await instance.enrolled(contractOwner, {
+      from: contractOwner,
+    });
     assert.equal(
       ownerEnrolled,
       false,
-      "only enrolled users should be marked enrolled",
+      "only enrolled users should be marked enrolled"
     );
   });
 
@@ -66,7 +68,7 @@ contract("SimpleBank", function (accounts) {
     assert.equal(
       deposit.toString(),
       balance,
-      "deposit amount incorrect, check deposit method",
+      "deposit amount incorrect, check deposit method"
     );
   });
 
@@ -82,13 +84,13 @@ contract("SimpleBank", function (accounts) {
     assert.equal(
       expectedEventResult.accountAddress,
       logAccountAddress,
-      "LogDepositMade event accountAddress property not emitted, check deposit method",
+      "LogDepositMade event accountAddress property not emitted, check deposit method"
     );
 
     assert.equal(
       expectedEventResult.amount,
       logDepositAmount,
-      "LogDepositMade event amount property not emitted, check deposit method",
+      "LogDepositMade event amount property not emitted, check deposit method"
     );
   });
 
@@ -102,7 +104,7 @@ contract("SimpleBank", function (accounts) {
     assert.equal(
       balance.toString(),
       initialAmount.toString(),
-      "balance incorrect after withdrawal, check withdraw method",
+      "balance incorrect after withdrawal, check withdraw method"
     );
   });
 
@@ -131,17 +133,17 @@ contract("SimpleBank", function (accounts) {
     assert.equal(
       expectedEventResult.accountAddress,
       accountAddress,
-      "LogWithdrawal event accountAddress property not emitted, check deposit method",
+      "LogWithdrawal event accountAddress property not emitted, check deposit method"
     );
     assert.equal(
       expectedEventResult.newBalance,
       newBalance,
-      "LogWithdrawal event newBalance property not emitted, check deposit method",
+      "LogWithdrawal event newBalance property not emitted, check deposit method"
     );
     assert.equal(
       expectedEventResult.withdrawAmount,
       withdrawAmount,
-      "LogWithdrawal event withdrawalAmount property not emitted, check deposit method",
+      "LogWithdrawal event withdrawalAmount property not emitted, check deposit method"
     );
   });
 });
